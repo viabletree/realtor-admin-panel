cc7c71c2813b5baac243162dbc0861912b2a00f5
msaadalam <64380190+msaadalam@users.noreply.github.com>
2022-08-11 20:35:24 +0500

    fixes

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 const PROD_URL = 'https://rlty-proxy.herokuapp.com/';
 const DEV_URL = 'https://rlty-proxy.herokuapp.com/';
- //const LOCALHOST_URL = 'https://rlty-proxy.herokuapp.com/';
+ const LOCALHOST_URL = 'https://rlty-proxy.herokuapp.com/';
 //const LOCALHOST_URL = 'https://prod.myrlty.com/';
-const LOCALHOST_URL = 'http://localhost:8001/';
-=======
-const PROD_URL = 'http://localhost:8000/';
-const DEV_URL = 'http://localhost:8000/';
-const LOCALHOST_URL = 'http://localhost:8000/';
-
->>>>>>> main
+//const LOCALHOST_URL = 'http://localhost:8001/';
 export const BASE_URL = (process.env.REACT_APP_ENVIRONMENT === 'production' ? PROD_URL : (process.env.REACT_APP_ENVIRONMENT === 'staging' ? DEV_URL : LOCALHOST_URL)) + 'api/v1/admin';
 
 export const RESOURCES = {
@@ -19,13 +12,12 @@ export const RESOURCES = {
   property_buyers: 'property_buyers',
   property_sellers: 'property_sellers',
   notes: 'notes',
-<<<<<<< HEAD
   faqs: 'faqs',
   setting: 'setting',
   
-=======
+
   showings: 'showings',
->>>>>>> main
+
 };
 
 /**
