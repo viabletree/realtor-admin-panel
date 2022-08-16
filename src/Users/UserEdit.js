6f9e69912a8fe86110f0d87564e7562b4d9289a3
mfaizyab <tasneem.faizyab@viabletree.com>
2022-08-16 15:46:20 +0500

    removed testing validation

--- src/Users/UserEdit.js
+++ src/Users/UserEdit.js
@@ -24,49 +24,48 @@ const UserShowActions = ({ basePath }) => (
     <ListButton basePath={basePath} label="Go Back to List" />
   </TopToolbar>
 );
-  
+
 const UserTitle = ({ record }) => {
   return record && record.name && (
     <span>{record.name}</span>
   );
 };
-  const duckyou = values => {
-    const errors = [];
-    errors.full_name = {
-      message: 'dash'
-    }
-    return errors;
-  }
-  const UserEdit = (props) => (
-    <Edit {...props} undoable={false}   actions={<UserShowActions/>} successMessage="User updated successfully">
+// const duckyou = values => {
+//   const errors = [];
+//   errors.full_name = {
+//     message: 'dash'
+//   }
+//   return errors;
+// }
+const UserEdit = (props) => (
+  <Edit {...props} undoable={false} actions={<UserShowActions />} successMessage="User updated successfully">
 
-      <SimpleForm validate={duckyou}>
-              <TextInput disabled label="Id" source="id"/>
-              <TextInput inputProps={{ maxLength: 40 }} multiline={true} source="full_name" validate={[required()]}/>
-              <TextInput inputProps={{ maxLength: 30 }} multiline={true} source="agency_name" validate={[required()]}/>
-              <TextInput inputProps={{ maxLength: 50 }} multiline={true} source="location" validate={[required()]}/>
-              <TextInput source="availability_from" validate={[required()]}/>
-              <TextInput source="availability_to" validate={[required()]}/>
-              <TextInput  multiline={true} source="bio" inputProps={{ maxLength: 255 }} validate={[required()]}/>
-             
-        {/* <TextInput source="users" validate={[required()]} /> */}
-      </SimpleForm>
-    </Edit>
-  );
-  
-  UserShowActions.propTypes = {
-    basePath: PropTypes.string
-  };
+    <SimpleForm >
+      <TextInput disabled label="Id" source="id" />
+      <TextInput inputProps={{ maxLength: 40 }} multiline={true} source="full_name" validate={[required()]} />
+      <TextInput inputProps={{ maxLength: 30 }} multiline={true} source="agency_name" validate={[required()]} />
+      <TextInput inputProps={{ maxLength: 50 }} multiline={true} source="location" validate={[required()]} />
+      <TextInput source="availability_from" validate={[required()]} />
+      <TextInput source="availability_to" validate={[required()]} />
+      <TextInput multiline={true} source="bio" inputProps={{ maxLength: 255 }} validate={[required()]} />
 
-  UserEdit.propTypes = {
-    record: PropTypes.object,
-    label: PropTypes.string,
-  };
+      {/* <TextInput source="users" validate={[required()]} /> */}
+    </SimpleForm>
+  </Edit >
+);
 
-  UserTitle.propTypes = {
-    record: PropTypes.object,
-    label: PropTypes.string,
-  };
-  
-  export default UserEdit;
-  
+UserShowActions.propTypes = {
+  basePath: PropTypes.string
+};
+
+UserEdit.propTypes = {
+  record: PropTypes.object,
+  label: PropTypes.string,
+};
+
+UserTitle.propTypes = {
+  record: PropTypes.object,
+  label: PropTypes.string,
+};
+
+export default UserEdit;
