d76605cf1f462c9caa75edde909071a1a757f8aa
zain-raza-viabletree <zain.raza@viabletree.com>
2022-08-31 13:34:20 +0500

    all base urls changed

--- src/constants.js
+++ src/constants.js
@@ -1,28 +1,29 @@
-const PROD_URL = 'https://rlty-proxy.herokuapp.com/';
-const DEV_URL = 'https://rlty-proxy.herokuapp.com/';
-const LOCALHOST_URL = 'https://rlty-proxy.herokuapp.com/';
-// const LOCALHOST_URL = 'https://prod.myrlty.com/';
-// const LOCALHOST_URL = 'http://localhost:8001/';
-export const BASE_URL = (process.env.REACT_APP_ENVIRONMENT === 'production' ? PROD_URL : (process.env.REACT_APP_ENVIRONMENT === 'staging' ? DEV_URL : LOCALHOST_URL)) + 'api/v1/admin';
+const PROD_URL = "https://dev.myrlty.com/";
+const DEV_URL = "https://dev.myrlty.com/";
+const LOCALHOST_URL = "https://dev.myrlty.com/";
+export const BASE_URL =
+  (process.env.REACT_APP_ENVIRONMENT === "production"
+    ? PROD_URL
+    : process.env.REACT_APP_ENVIRONMENT === "staging"
+    ? DEV_URL
+    : LOCALHOST_URL) + "api/v1/admin";
 
 export const RESOURCES = {
-  users: 'users',
-  properties: 'properties',
-  property_types: 'property_types',
-  property_buyers: 'property_buyers',
-  property_sellers: 'property_sellers',
-  notes: 'notes',
-  faqs: 'faqs',
-  setting: 'setting',
+  users: "users",
+  properties: "properties",
+  property_types: "property_types",
+  property_buyers: "property_buyers",
+  property_sellers: "property_sellers",
+  notes: "notes",
+  faqs: "faqs",
+  setting: "setting",
 
-
-  showings: 'showings',
-
+  showings: "showings",
 };
 
 /**
- * 
- * @param {String} resourceName 
+ *
+ * @param {String} resourceName
  * @returns {String}
  */
 export function getResourcePath(resourceName) {
