f466f19c9c2e0ea8db5758d78508cf778c412ae4
zain-raza-viabletree <zain.raza@viabletree.com>
2022-08-31 19:52:47 +0500

    [Ticket #270] [Property Buyer List Property Seller List]: [create - amount of contract field should not show up and down button] Commit

--- src/Users/UserCreate.js
+++ src/Users/UserCreate.js
@@ -1,3 +1,4 @@
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import {
   Create,
   SimpleForm,
@@ -9,32 +10,95 @@ import {
   PasswordInput,
   ReferenceInput,
   AutocompleteInput,
-  TextField
-} from 'react-admin';
-const validateName = [required('Name is required')];
-const validateEmail = [required('Email is required'), email('Incorrect Email')];
-const validatePassword = [required('Password is required'), minLength(6)];
+  TextField,
+  DateTimeInput,
+} from "react-admin";
+import { TimeInput } from "react-admin-date-inputs2";
+import DateFnsUtils from "@date-io/date-fns";
+
+const validateName = [required("Name is required")];
+const validateEmail = [required("Email is required"), email("Incorrect Email")];
+const validatePassword = [required("Password is required"), minLength(6)];
 
 const UserCreate = (props) => (
   <Create {...props} successMessage="User created successfully">
     <SimpleForm>
-    <TextInput source="full_name" inputProps={{ maxLength:100 }} validate={[required()]}/>
-              <TextInput source="email" inputProps={{ maxLength: 100 }} validate={[required()]}/>
-              <TextInput label="Password" source="password" inputProps={{ maxLength: 100 }} validate={[required()]}/>
-              <TextInput label="Confirm Password" source="confirm_password" inputProps={{ maxLength: 100 }} validate={[required()]}/>
-              <NumberInput label="Phone Number" source="phone" inputProps={{ maxLength: 100 }} validate={[required()]}/>
-              <TextInput multiline={true} label="Agency Name" inputProps={{ maxLength: 100 }} source="agency_name" validate={[required()]}/>
-              <TextInput label="Location" source="location" inputProps={{ maxLength: 100 }} validate={[required()]}/>
-              <TextInput label="Availability From" source="availability_from" validate={[required()]}/>
-              <TextInput label="Availability To" source="availability_to" validate={[required()]}/>
-              <TextInput multiline={true}label="Bio" source="bio" inputProps={{ maxLength: 255 }} validate={[required()]}/>
-             
-            {/*   <TextInput source="agency_name" validate={[required()]}/>
+      <TextInput
+        source="full_name"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      <TextInput
+        source="email"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      <TextInput
+        label="Password"
+        source="password"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      <TextInput
+        label="Confirm Password"
+        source="confirm_password"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      <NumberInput
+        label="Phone Number"
+        source="phone"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      <TextInput
+        multiline={true}
+        label="Agency Name"
+        inputProps={{ maxLength: 100 }}
+        source="agency_name"
+        validate={[required()]}
+      />
+      <TextInput
+        label="Location"
+        source="location"
+        inputProps={{ maxLength: 100 }}
+        validate={[required()]}
+      />
+      {/* <TextInput label="Availability From" source="availability_from" validate={[required()]}/>
+              <TextInput label="Availability To" source="availability_to" validate={[required()]}/> */}
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <TimeInput
+          source="availability_from"
+          label="Availability From"
+          options={{ format: "hh:mm:ss" }}
+          validate={[required()]}
+          inputProps={{ maxLength: 255 }}
+        />
+      </MuiPickersUtilsProvider>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <TimeInput
+          source="availability_to"
+          label="Availability To"
+          options={{ format: "hh:mm:ss", variant: "filled" }}
+          validate={[required()]}
+          inputProps={{ maxLength: 255, variant: "filled" }}
+        />
+      </MuiPickersUtilsProvider>
+      <TextInput
+        multiline={true}
+        label="Bio"
+        source="bio"
+        inputProps={{ maxLength: 255 }}
+        validate={[required()]}
+      />
+
+      {/*   <TextInput source="agency_name" validate={[required()]}/>
               <TextInput source="location" validate={[required()]}/>
               <TextInput source="availability_from" validate={[required()]}/>
               <TextInput source="availability_to" validate={[required()]}/>
               <TextInput source="bio" validate={[required()]}/>
-             */} 
+             */}
     </SimpleForm>
   </Create>
 );
